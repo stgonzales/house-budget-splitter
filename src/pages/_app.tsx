0479eeadb7e2887cd03d857460ae7b638d025f16
Stephen Goncalves <stiefo.o@gmail.com>
2023-04-11 21:17:57 +0100

    removed store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,19 +5,15 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
-import { Provider } from "react-redux";
-import { store } from "@/store";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <Provider store={store}>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </Provider>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 };
 
